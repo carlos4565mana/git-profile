3cd3784ed6e460d5ab7cb5c1afa886b2d8b59873
carlos4565mana <carloscal61@gmail.com>
2022-07-12 22:04:10 -0300

    created Users component

--- desafio-react/src/App.jsx
+++ desafio-react/src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import Logo from './components/Logo'
+import Users from './components/Users'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className=''>
+    <div className="min-h-screen bg-black">
+      <div className="container text-gray-200 py-3">
       <Logo />
+      <Users />
 
+      </div>
+     
     </div>
-    
   )
 }
 
---
+++ desafio-react/src/components/Users.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect, useRef } from "react";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(null);
+  const user = useRef("");
+
+  let BaseURL = "https://api.github.com/users";
+
+  async function allUsers() {
+    if(user.current.value === ""){
+      setLoading(true);
+      const res = await fetch(BaseURL);
+      const data = await res.json();
+      setUsers(data);
+      setLoading(null);
+    }
+  }
+
+  async function findUser() {
+    setLoading(true);
+    if(user.current.value !== ""){
+      setUsers("");
+      const res = await fetch(BaseURL + "/" + user.current.value);
+      const data=  await res.json();
+      setUsers(()=>[data]);
+      user.current.value = "";
+    }
+    setLoading(null);
+
+  }
+
+  useEffect( () => {
+    allUsers();
+
+  }, [user,setUsers])
+  return (
+    <div>
+      <div className="flex justify-center h-11  my-5 items-center">
+        <input
+          ref={user} 
+          placeholder="Search github username"
+          type="text"
+          className="h-full md:w-1/3 outline-none text-gray-800 px-2 
+          font-semibold text-lg w-2/3" />
+          <button 
+            onClick={findUser}
+            className="bg-teal-500 font-semibold  px-4 h-full font-[Poppins]"
+           >
+            Search
+          </button>
+      </div>
+   </div>
+  )
+}
+export default Users;
